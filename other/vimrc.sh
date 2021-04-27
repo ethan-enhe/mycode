c10e482291722b3d4d9ce107bf9939cacefe9d14
ethan-enhe <18201521626@139.com>
2021-04-27 19:29:01 +0800

    improve vimscript

--- other/vimrc.sh
+++ other/vimrc.sh
@@ -1,3 +1,7 @@
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath = &runtimepath\nsource ~/.vimrc" >> init.vim
+mkdir -p ~/.config/nvim
+mv init.vim ~/.config/nvim/init.vim
+
 wget blog-e.tk/file/vimrc
 wget blog-e.tk/file/plug.vim
 mkdir -p  ~/.vim/autoload/
