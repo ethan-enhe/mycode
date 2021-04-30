e6480f5973015a03b9cd26413b8381cead9eae9f
18201521626@139.com <18201521626@139.com>
2021-04-30 13:19:11 +0800

    更新文件 main.cpp

--- fun/gomoku/main.cpp
+++ fun/gomoku/main.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int SZ=15;
+const int GO[8][2]={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+
 struct pr{
 	int x,y;
 	pr(int x=0,int y=0):x(x),y(y){}
@@ -8,18 +10,16 @@ struct pr{
 
 
 inline bool inbrd(pr co){return co.x>0 && co.y>0 && co.x<=SZ && co.y<=SZ;}
-
-#define va(a) arr[a.x][a.y]
 struct board{
 	//0=空，-1=白，1=黑
 	//turn表示下一子该谁下
 	int arr[SZ+1][SZ+1],turn;
-	board(){memset(arr,0,sizeof(arr));turn=1;}
+	board(){turn=1;memset(arr,0,sizeof(arr));}
 
 	//传坐标返回是否成功
 	bool put(pr co){
-		if(!inbrd(co) || va(co))return 0;
-		va(co)=turn;turn=-turn;
+		if(!inbrd(co) || arr[co.x][co.y])return 0;
+		arr[co.x][co.y]=turn;turn=-turn;
 		return 1;
 	}
 	void prt(){
@@ -30,18 +30,31 @@ struct board{
 	}
 };
 
-
-
 struct board_sta{
 	//a前缀表示活，r前缀表示眠/冲
-	int five,afour,rfour,athree,rthree,atwo,rtwo;
+	double five,afour,rfour,athree,rthree,atwo,rtwo;
 	board_sta(){five=afour=rfour=athree=rthree=atwo=rtwo=0;}
 };
 
-inline bool match(){
+inline bool chkfive(int arr[SZ+1][SZ+1],pr co,int turn){
+    if(arr[co.x][co.y]!=turn)return 0;
+    for(int i=0;i<8;i++){
+        bool f=1;pr tco=co;
+        for(int j=1;j<=4;j++){
+            tco.x+=GO[i][0],tco.y+=GO[i][1];
+            if(arr[tco.x][tco.y]!=turn){
+                f=0;
+                break;
+            }
+        }
+        if(f)return 1;
+    }
+    return 0;
 }
 
-int main(){
 
+
+
+int main(){
 	return 0;
 }
