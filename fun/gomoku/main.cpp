a231059a611a57c796ad6e7e32fd779514bf692c
ethan-enhe <18201521626@139.com>
2021-05-26 21:23:03 +0800

    opt

--- fun/gomoku/main.cpp
+++ fun/gomoku/main.cpp
@@ -1,3 +1,46 @@
+#pragma GCC optimize(3)
+#pragma GCC target("avx,sse2,sse3,sse4,mmx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
 #include<bits/stdc++.h>
 using namespace std;
 typedef unsigned long long ull;
@@ -147,7 +190,8 @@ inline void matchline(const board &cbd,state &res,pr start,pr move,int side){
 
 
 inline state cal(const board &cbd,int side){
-	state &res=side==1?saved[cbd.hsh].bst:saved[cbd.hsh].wst;
+	//state &res=side==1?saved[cbd.hsh].bst:saved[cbd.hsh].wst;
+	state res;
 	if(!res.notcal)return res;
 	res.notcal=0;
 	for(int i=1;i<=SZ;i++){
@@ -186,7 +230,7 @@ int main(){
 	tmp.put(pr(9,8));
 	tmp.put(pr(7,5));
 	int res=0;
-	for(int i=1;i<=2000000;i++){
+	for(int i=1;i<=200000;i++){
 		state x=cal(tmp,-1);
 		res+=x.defa3.size();
 	}
