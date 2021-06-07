2fc7850f2ab9aedb060453582af0249849349f4d
ethan-enhe <18201521626@139.com>
2021-06-07 08:02:22 +0800

    newcf

---
+++ contest/cf/1536/a/a.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll,ll> pi;
+
+const ll INF=1e18;
+const ll P=1e9+7;
+
+inline ll mod(ll x){return x>=P?x-P:x;}
+inline ll mul(ll x,ll y){if(x<y)swap(x,y);ll res=0;while(y){if(y&1)res=mod(res+x);x=mod(x<<1);y>>=1;}return res;}
+inline ll qpow(ll x,ll y){ll res=1;while(y){if(y&1)res=res*x%P;x=x*x%P;y>>=1;}return res;}
+ll gcd(ll x,ll y){return !y?x:gcd(y,x%y);}
+inline void pbin(ll x,ll y){for(;~y;y--)cerr<<char('0'+((x>>y)&1));cerr<<endl;}
+
+
+const ll MXN=1e6+5;
+
+ll n,m,k;
+ll arr[MXN];
+inline void solve(){
+	scanf("%lld",&n);
+	ll mx=-INF,mn=INF,g=0;
+	for(int i=1;i<=n;i++){
+		scanf("%lld",arr+i);
+		g=gcd(g,arr[i]);
+		mn=min(arr[i],mn);
+		mx=max(arr[i],mx);
+	}
+	g=abs(g);
+	if(mn<0)printf("NO");
+	else{
+		ll cnt=0;
+		for(int i=min(mn,g);i<=mx;i+=g)cnt++;
+		printf("YES\n%lld\n",cnt);
+		for(int i=min(mn,g);i<=mx;i+=g)printf("%d ",i);
+	}
+	cout<<endl;
+    
+}
+int main(){
+	//freopen(".in","r",stdin);
+	//freopen(".out","w",stdout);
+	ll tq;scanf("%lld",&tq);while(tq--)
+	solve();
+	return 0;
+}
