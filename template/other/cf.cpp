00f05656170965bd2d93e73e30f102d010236602
ethan-enhe <18201521626@139.com>
2021-06-10 21:05:36 +0800

    fix cf template

--- template/other/cf.cpp
+++ template/other/cf.cpp
@@ -14,7 +14,7 @@ const ll P=1e9+7;
 inline ll mod(ll x){return x>=P?x-P:x;}
 inline ll mul(ll x,ll y){if(x<y)swap(x,y);ll res=0;while(y){if(y&1)res=mod(res+x);x=mod(x<<1);y>>=1;}return res;}
 inline ll qpow(ll x,ll y){ll res=1;while(y){if(y&1)res=res*x%P;x=x*x%P;y>>=1;}return res;}
-ll gcd(ll x,ll y){return !y?x:gcd(y,x%y);}
+inline ll gcd(ll x,ll y){return !y?x:gcd(y,x%y);}
 inline void pbin(ll x,ll y){for(;~y;y--)cerr<<char('0'+((x>>y)&1));cerr<<endl;}
 
 
