09f6a62e897a673dc8416f325d59051f544ee300
ethan-enhe <18201521626@139.com>
2021-06-19 21:31:22 +0800

    del

--- other/vimrc.sh
+++
@@ -1,9 +0,0 @@
-echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath = &runtimepath\nsource ~/.vimrc" >> init.vim
-mkdir -p ~/.config/nvim
-mv init.vim ~/.config/nvim/init.vim
-
-wget blog-e.tk/file/vimrc
-wget blog-e.tk/file/plug.vim
-mkdir -p  ~/.vim/autoload/
-mv plug.vim  ~/.vim/autoload/plug.vim
-mv vimrc ~/.vimrc
