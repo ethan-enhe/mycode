958ae9ddf2334952a1e573487ef92ed72debc2eb
ethan-enhe <18201521626@139.com>
2021-07-09 16:19:48 +0800

    fix

--- fun/pi2frac/block.cpp
+++ fun/pi2frac/block.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 typedef long double ld;
 //const ll MXN=1e10;
 const ll PROG=(1ll<<30)-1;
-const ll MXN=1e11;
+const ll MXN=1e12;
 const ll SQRT=sqrt(MXN)+5;
 const ll BSZ=SQRT;
 inline ld lfabs(const ld &x){return x>0?x:-x;}
@@ -53,7 +53,7 @@ int main(){
 	for(ll i=1;i<MXN;i++,r.nx()){
 		if(!(i&PROG)){
 	        printf("%.18LF\n%lld %lld %.18Lf %.18Lf\n",(ld)M_PI,s,m,(ld)s/m,mn);
-            printf("%.3Lf sec %lld\%\n",ld(clock()-start)/CLOCKS_PER_SEC,i*100/MXN);
+            printf("%.3LfSecs %.2lf\%\n",ld(clock()-start)/CLOCKS_PER_SEC,100.*i/MXN);
         }
 		if(r.ispri()){
 			while(err(i,nx)<=err(i,cur)){
