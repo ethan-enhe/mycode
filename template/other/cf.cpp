b3748bc531aced04ff6a8790e0bb55ff68ada276
18201521626@139.com <18201521626@139.com>
2021-08-20 15:17:45 +0800

    更新文件 cf.cpp

--- template/other/cf.cpp
+++ template/other/cf.cpp
@@ -1,34 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//{{{ Def
+#define il inline
 #define fi first
 #define se second
-#define pb push_back
 #define mp make_pair
+#define pb push_back
+#define dbg(x) cerr<<#x<<"="<<(x)<<endl;
+#define inv(x) qpow(x,P-2)
+
+typedef double db;
+typedef long double ld;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<ll,ll> pi;
-
-const ll INF=1e18;
-const ll P=1e9+7;
-
-inline ll mod(ll x){return x>=P?x-P:x;}
-inline ll mul(ll x,ll y){if(x<y)swap(x,y);ll res=0;while(y){if(y&1)res=mod(res+x);x=mod(x<<1);y>>=1;}return res;}
-inline ll qpow(ll x,ll y){ll res=1;while(y){if(y&1)res=res*x%P;x=x*x%P;y>>=1;}return res;}
-inline ll gcd(ll x,ll y){return !y?x:gcd(y,x%y);}
-inline void pbin(ll x,ll y){for(;~y;y--)cerr<<char('0'+((x>>y)&1));cerr<<endl;}
+typedef vector<ll> vi;
+//}}}
 
 
-const ll MXN=1e6+5;
+//{{{ Const
+const char nl='\n';
+const db EPS=1e-9;
+const ull B=131;
+const ll INF=1e18,P=1e9+7;
+//}}}
 
+
+
+//{{{ Func
+il ll qpow(ll x,ll y){
+    ll r=1;
+    while(y){
+        if(y&1)r=r*x%P;
+        x=x*x%P,y>>=1;
+    }
+    return r;
+}
+il ll gcd(ll x,ll y){return !y?x:gcd(y,x%y);}
+il ll mod(ll x){
+	if(x>=P)return x-P;
+	if(x<0)return x+P;
+	return x;
+}
+il void add(ll &x,ll y){x=mod(x+y);}
+template<class T>il void cmx(T &x,T y){x=max(x,y);}
+template<class T>il void cmn(T &x,T y){x=min(x,y);}
+//}}}
+
+
+const ll MXN=5e5+5;
 ll n,m,k;
 ll arr[MXN];
-inline void solve(){
-    
+
+il void solve(){
+
 }
+
+
 int main(){
-	//freopen(".in","r",stdin);
-	//freopen(".out","w",stdout);
-	ll tq;scanf("%lld",&tq);while(tq--)
+	//freopen(".in","r",stdin),freopen(".out","w",stdout);
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	//cin>>t;while(t--)
 	solve();
+
 	return 0;
 }
