22b8a5de26815409a86ba42b6fd57733c40d2ac8
ethan_enhe <18201521626@139.com>
2022-01-02 21:22:29 +0800

    newcode

--- prob/test.cpp
+++ prob/test.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 //#include <bits/extc++.h>
 using namespace std;
+void a(){
+}
 int main(){
     int asdaa;
     cin >> asdaa;
     cout << asdaa << endl;
+	asdaa;
     return 0;
 }
