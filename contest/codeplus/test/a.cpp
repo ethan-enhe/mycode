4ece096d6f582c2d3bf99041a804f599eaff505e
ethan_enhe <18201521626@139.com>
2022-10-01 20:15:56 +0800

    asdas

---
+++ contest/codeplus/test/a.cpp
@@ -0,0 +1,121 @@
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,avx,avx2,bmi,bmi2,lzcnt,popcnt")
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << endl
+#else
+#define dbg(...) 42
+#define NDEBUG
+#endif
+#include <bits/stdc++.h>
+
+using namespace std;
+//{{{ Def
+#define fi first
+#define se second
+#define vec vector
+#define all(x) (x).begin(), (x).end()
+#define unq(x) (x).erase(unique(all(x)), (x).end())
+#define tpl template <typename T>
+
+using ll = long long;
+using ull = unsigned long long;
+using db = double;
+using ld = long double;
+using pi = pair<ll, ll>;
+//}}}
+//{{{ Func
+tpl pair<T, T> &operator+=(pair<T, T> &x, const pair<T, T> &y) { return x.fi += y.fi, x.se += y.se, x; }
+tpl pair<T, T> operator+(const pair<T, T> &x, const pair<T, T> &y) { return {x.fi + y.fi, x.se + y.se}; }
+tpl pair<T, T> &operator-=(pair<T, T> &x, const pair<T, T> &y) { return x.fi -= y.fi, x.se -= y.se, x; }
+tpl pair<T, T> operator-(const pair<T, T> &x, const pair<T, T> &y) { return {x.fi - y.fi, x.se - y.se}; }
+tpl pair<T, T> &operator*=(pair<T, T> &x, const ll &y) { return x.fi *= y, x.se *= y, x; }
+tpl pair<T, T> operator*(const pair<T, T> &x, const ll &y) { return {x.fi * y, x.se * y}; }
+tpl istream &operator>>(istream &is, pair<T, T> &y) { return is >> y.fi >> y.se; }
+tpl ostream &operator<<(ostream &os, const pair<T, T> &y) { return os << '(' << y.fi << ',' << y.se << ')'; }
+tpl T qpow(T x, ll y) {
+    T r(1);
+    while (y) {
+        if (y & 1) r = r * x;
+        x = x * x, y >>= 1;
+    }
+    return r;
+}
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    if (!a || !b) return a | b;
+    ll U = __builtin_ctzll(a), V = __builtin_ctzll(b);
+    a >>= U, b >>= V;
+    if (U > V) U = V;
+    while (a) {
+        if (a < b) swap(a, b);
+        a -= b;
+        a >>= __builtin_ctzll(a);
+    }
+    return b << U;
+}
+tpl void umx(T &x, const T &y) { x = max(x, y); }
+tpl void umn(T &x, const T &y) { x = min(x, y); }
+bool inrng(const ll &x, const ll &l, const ll &r) { return l <= x && x <= r; }
+bool insqr(const pi &x, const pi &lt, const pi &rb) {
+    return lt.fi <= x.fi && x.fi <= rb.fi && lt.se <= x.se && x.se <= rb.se;
+}
+void setp(const ll &x) {
+    cout.flags(ios::fixed);
+    cout.precision(x);
+}
+template <typename T = ll>
+T nxt() {
+    T x;
+    cin >> x;
+    return x;
+}
+mt19937_64 mr(chrono::system_clock::now().time_since_epoch().count());
+ll ri(const ll &l, const ll &r) { return uniform_int_distribution<ll>(l, r)(mr); }
+ld rd(const ld &l, const ld &r) { return uniform_real_distribution<ld>(l, r)(mr); }
+//}}}
+const char nl = '\n';
+const ll INF = 1e16;
+const ll MXN = 1e6 + 5;
+
+ll n, m, arr[MXN];
+
+struct num {
+    ll v;
+    bool f;
+    num(ll _v = 0) {
+        v = _v;
+        f = 0;
+    }
+    num &operator+=(const num &b) {
+        f |= b.f;
+        v += b.v;
+        if (v >= INF) {
+            v -= INF;
+            f = 1;
+        }
+        return *this;
+    }
+};
+
+num dp[MXN][4];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    dp[1][1] = 3;
+    for (ll i = 1; i < n; i++) {
+        for (ll j = 1; j < 3; j++) {
+            dp[i + 1][j + 1] += dp[i][j];
+            dp[i + 1][1] += dp[i][j];
+            dp[i + 1][1] += dp[i][j];
+        }
+    }
+    dp[n][1] += dp[n][2];
+    if (dp[n][1].f) {
+        cout << "......" << dp[n][1].v % ll(1e10);
+    } else
+        cout << dp[n][1].v;
+    return 0;
+}
